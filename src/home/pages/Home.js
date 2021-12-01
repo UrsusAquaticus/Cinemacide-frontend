0639d9870e663ea99c0dce0d8097e909765fe1a6
Zach <Zach.hucker@gmail.com>
2021-12-02 12:01:39 +1300

    Removed Cards

--- src/home/pages/Home.js
+++ src/home/pages/Home.js
@@ -1,9 +1,6 @@
 import React, { useState, useContext } from "react";
 import { makeStyles } from "@material-ui/core";
 
-import { useHttpClient } from "../../shared/hooks/http-hook";
-import { useEffect } from "react";
-
 import { Grid, Paper } from "@material-ui/core";
 import { Card } from "@material-ui/core";
 import { CardContent } from "@material-ui/core";
@@ -11,8 +8,6 @@ import { Typography } from "@material-ui/core";
 
 // import AuthDialog from "../../shared/components/Navigation/AuthDialog";
 // import { AuthContext } from "../../shared/context/auth-context";
-
-import { useSnackbar } from "notistack";
 
 const useStyles = makeStyles({
 	card: {
@@ -37,27 +32,6 @@ const Home = (props) => {
 	// 	setAuthOpen(!authOpen);
 	// };
 	const styles = useStyles();
-	const [loadedCards, setLoadedCards] = useState([]);
-	const { isLoading, error, sendRequest, clearError } = useHttpClient();
-	const { enqueueSnackbar, closeSnackbar } = useSnackbar();
-
-	useEffect(() => {
-		const fetchCards = async () => {
-			try {
-				const responseData = await sendRequest(
-					`https://api.scryfall.com/cards/search?q=year%3E%3D2021+include%3Aextras+unique%3Aprints&order=spoiled&as=grid&unique=prints`
-				);
-
-				setLoadedCards(responseData.data);
-			} catch (err) {
-				enqueueSnackbar("Failed to Fetch Cards: " + err.message, {
-					variant: "error",
-				});
-			}
-		};
-		fetchCards();
-	}, [sendRequest]);
-
 	return (
 		<React.Fragment>
 			{/* <AuthDialog authOpen={authOpen} toggleAuthOpen={toggleAuthOpen} /> */}
@@ -76,30 +50,6 @@ const Home = (props) => {
 					</CardContent>
 				</Card>
 			</Grid>
-			{loadedCards &&
-				!isLoading &&
-				loadedCards.map((card, ci) => (
-					<Card className={styles.card} id={"card-" + ci}>
-						{console.log(card.name)}
-						<Typography>{card.name}</Typography>
-						<a href={card.scryfall_uri}>
-							{"image_uris" in card ? (
-								<img
-									className={styles.image}
-									src={card.image_uris.normal || "/"}
-								/>
-							) : (
-								card.card_faces.map((face, fi) => (
-									<img
-										className={styles.image}
-										id={"face-" + fi}
-										src={face.image_uris.normal || "/"}
-									/>
-								))
-							)}
-						</a>
-					</Card>
-				))}
 		</React.Fragment>
 	);
 };
